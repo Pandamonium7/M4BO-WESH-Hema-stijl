3ec17d99429576e69872bc5914bda66a3a01c212
Arturo <38075@ma-web.nl>
2024-06-21 06:49:33 +0200

    wat producten toegevoegd voor resposiveness

--- script.php
+++ script.php
@@ -1,31 +1,32 @@
 <?php
 
+//product_ID, product_title, product_brand, product_prijs, product_desc, product_img, product_category, product_color
+
+// echo $row["product_ID"].   $row["product_title"]. $row["product_brand"].  $row["product_prijs"].
+//      $row["product_desc"]. $row["product_img"]. $row["product_category"]. $row["product_color"]     
+
 include_once("../source/database.php");
 
 $connection = database_connect();
-
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-  } else {
     
-    $sql = "SELECT product_ID, product_title, product_brand, product_prijs, product_desc FROM product";
+    $sql = "SELECT * FROM product";
     $result = $connection->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["product_ID"]. " - Titel: " . $row["product_title"]. " - Merk: ". $row["product_brand"]. " - Prijs: € " . $row["product_prijs"]. "<br>" 
-            . "Beschrijving: " . $row["product_desc"] . "<br><br><br>";
+        echo "id: " . $row["product_ID"]. "<br><br> - Titel: " . $row["product_title"]. "<br> - Merk: ". $row["product_brand"]. "<br> - Prijs: € " 
+                    . $row["product_prijs"]. "<br><br> - Beschrijving: " . $row["product_desc"] . "<br><br> - Afbeeldingen: ". $row["product_img"]
+                    . "<br> - Categorie: ". $row["product_category"]. "<br> - Kleur: ". $row["product_color"]. "<br><br>";
         }
     } else {
         echo "0 results";
         }
     $connection->close();
-    }
+
     
     
-//$result = $connection->query("SELECT * FROM product");
 
 
 //print_r($result->fetch_all());
-?>
+
